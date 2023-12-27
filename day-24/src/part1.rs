c1e8244cd914557ad815d9692f31b7e1b4c1965c
Johannes Holzfuß <johannes@holzfuss.name>
2023-12-27 16:16:54 +0100

    Day 24 template

---
+++ day-24/src/part1.rs
@@ -0,0 +1,23 @@
+use crate::structs::*;
+
+use utility_belt::prelude::*;
+
+pub fn part1(input: &PuzzleInput) -> String {
+    todo!();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use utility_belt::prelude::*;
+
+    const TEST_INPUT: &str = indoc! {"
+        TODO
+    "};
+
+    #[test]
+    fn test_part1() {
+        let input = crate::parser::parse(TEST_INPUT);
+        assert_eq!(part1(&input), "TODO");
+    }
+}
