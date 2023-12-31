6630d150f9f10ccb1d36a44bbbfb436eed148846
Johannes Holzfuß <johannes@holzfuss.name>
2023-12-31 19:23:19 +0100

    Day 6 Part 1

--- 2022/day-06/src/parser.rs
+++ 2022/day-06/src/parser.rs
@@ -2,26 +2,8 @@ use utility_belt::prelude::*;
 
 use crate::structs::*;
 
-fn nom_parser(input: &str) -> IResult<&str, PuzzleInput> {
-    todo!();
-    let (input, _) = eof(input)?;
-
-    Ok((input, PuzzleInput {}))
-}
-
 pub fn parse(input: &str) -> PuzzleInput {
-    nom_parser(input).unwrap().1
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use utility_belt::prelude::*;
-
-    const TEST_INPUT: &str = include_str!("../test.txt");
-
-    #[test]
-    fn test_parse() {
-        assert!(nom_parser(TEST_INPUT).is_ok());
+    PuzzleInput {
+        sequence: input.chars().collect_vec(),
     }
 }
--- 2022/day-06/src/part1.rs
+++ 2022/day-06/src/part1.rs
@@ -3,7 +3,18 @@ use crate::structs::*;
 use utility_belt::prelude::*;
 
 pub fn part1(input: &PuzzleInput) -> String {
-    todo!();
+    (input
+        .sequence
+        .iter()
+        .as_slice()
+        .windows(4)
+        .enumerate()
+        .take_while(|(_, w)| HashSet::from_iter(w.iter().cloned()).len() != 4)
+        .last()
+        .unwrap()
+        .0
+        + 5)
+    .to_string()
 }
 
 #[cfg(test)]
@@ -16,6 +27,6 @@ mod tests {
     #[test]
     fn test_part1() {
         let input = crate::parser::parse(TEST_INPUT);
-        assert_eq!(part1(&input), "TODO");
+        assert_eq!(part1(&input), "7");
     }
 }
