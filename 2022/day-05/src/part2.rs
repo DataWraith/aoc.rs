0d0a3ac8c02a31d9031fa02d37de16c366fcd22d
Johannes Holzfuß <johannes@holzfuss.name>
2023-12-31 17:37:05 +0100

    Day 5, part 2

--- 2022/day-05/src/part2.rs
+++ 2022/day-05/src/part2.rs
@@ -3,7 +3,21 @@ use crate::structs::*;
 use utility_belt::prelude::*;
 
 pub fn part2(input: &PuzzleInput) -> String {
-    todo!();
+    let mut crates = input.crates.clone();
+
+    for (n, from, to) in input.instructions.iter() {
+        let mut stack = Vec::new();
+
+        for _ in 0..*n {
+            stack.push(crates[*from].pop().unwrap());
+        }
+
+        while let Some(c) = stack.pop() {
+            crates[*to].push(c);
+        }
+    }
+
+    crates.iter().filter_map(|c| c.last()).join("")
 }
 
 #[cfg(test)]
@@ -16,6 +30,6 @@ mod tests {
     #[test]
     fn test_part2() {
         let input = crate::parser::parse(TEST_INPUT);
-        assert_eq!(part2(&input), "TODO");
+        assert_eq!(part2(&input), "MCD");
     }
 }
