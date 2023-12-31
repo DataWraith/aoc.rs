6583cfdb78a29310156a43161825f7c81dc0f6dd
Johannes Holzfuß <johannes@holzfuss.name>
2023-12-31 19:31:41 +0100

    Day 6, Part 2

--- 2022/day-06/src/part2.rs
+++ 2022/day-06/src/part2.rs
@@ -3,7 +3,19 @@ use crate::structs::*;
 use utility_belt::prelude::*;
 
 pub fn part2(input: &PuzzleInput) -> String {
-    todo!();
+    (input
+        .sequence
+        .iter()
+        .as_slice()
+        .windows(14)
+        .enumerate()
+        .take_while(|(_, w)| HashSet::from_iter(w.iter().cloned()).len() != 14)
+        .last()
+        .unwrap()
+        .0
+        + 14
+        + 1)
+    .to_string()
 }
 
 #[cfg(test)]
@@ -16,6 +28,6 @@ mod tests {
     #[test]
     fn test_part2() {
         let input = crate::parser::parse(TEST_INPUT);
-        assert_eq!(part2(&input), "TODO");
+        assert_eq!(part2(&input), "19");
     }
 }
