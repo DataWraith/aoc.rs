a9657c7c99f81cb56794fe121d84bcb39a779fcb
Johannes Holzfuß <johannes@holzfuss.name>
2024-01-02 11:40:41 +0100

    Day 9, Part 1

--- 2022/day-09/src/parser.rs
+++ 2022/day-09/src/parser.rs
@@ -2,26 +2,17 @@ use utility_belt::prelude::*;
 
 use crate::structs::*;
 
-fn nom_parser(input: &str) -> IResult<&str, PuzzleInput> {
-    todo!();
-    let (input, _) = eof(input)?;
-
-    Ok((input, PuzzleInput {}))
-}
-
 pub fn parse(input: &str) -> PuzzleInput {
-    nom_parser(input).unwrap().1
-}
+    let moves = input
+        .lines()
+        .map(|line| {
+            let (dir, len) = line.split_once(' ').unwrap();
+            let direction: Direction = dir.chars().next().unwrap().try_into().unwrap();
+            let length = len.parse::<usize>().unwrap();
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use utility_belt::prelude::*;
+            (direction, length)
+        })
+        .collect();
 
-    const TEST_INPUT: &str = include_str!("../test.txt");
-
-    #[test]
-    fn test_parse() {
-        assert!(nom_parser(TEST_INPUT).is_ok());
-    }
+    PuzzleInput { moves }
 }
--- 2022/day-09/src/part1.rs
+++ 2022/day-09/src/part1.rs
@@ -3,7 +3,44 @@ use crate::structs::*;
 use utility_belt::prelude::*;
 
 pub fn part1(input: &PuzzleInput) -> String {
-    todo!();
+    visited_tiles(input).len().to_string()
+}
+
+pub fn step(head: Coordinate, tail: Coordinate, dir: Direction) -> (Coordinate, Coordinate) {
+    let new_head = head + dir.into();
+
+    if new_head == tail || new_head.moore_neighbors().contains(&tail) {
+        return (new_head, tail);
+    }
+
+    if tail.x() == new_head.x() || tail.y() == new_head.y() {
+        let new_tail = tail + tail.towards(new_head).into();
+        return (new_head, new_tail);
+    }
+
+    (new_head, head)
+}
+
+pub fn visited_tiles(input: &PuzzleInput) -> HashSet<Coordinate> {
+    let mut visited = HashSet::default();
+
+    let mut cur_head = Coordinate::new(0, 0);
+    let mut cur_tail = Coordinate::new(0, 0);
+
+    visited.insert(cur_tail);
+
+    for (direction, length) in input.moves.iter() {
+        for _ in 0..*length {
+            let (new_head, new_tail) = step(cur_head, cur_tail, *direction);
+
+            cur_head = new_head;
+            cur_tail = new_tail;
+
+            visited.insert(cur_tail);
+        }
+    }
+
+    visited
 }
 
 #[cfg(test)]
@@ -16,6 +53,6 @@ mod tests {
     #[test]
     fn test_part1() {
         let input = crate::parser::parse(TEST_INPUT);
-        assert_eq!(part1(&input), "TODO");
+        assert_eq!(part1(&input), "13");
     }
 }
