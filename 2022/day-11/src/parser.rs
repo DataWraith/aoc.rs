82d9d96950c53492fd368fb1a552bf8868e4f1d6
Johannes Holzfuß <johannes@holzfuss.name>
2024-01-06 13:33:49 +0100

    Day 11 parser done

--- 2022/day-11/src/parser.rs
+++ 2022/day-11/src/parser.rs
@@ -1,16 +1,75 @@
-use utility_belt::prelude::*;
+use utility_belt::prelude::{
+    nom::{combinator::opt, Parser},
+    *,
+};
 
 use crate::structs::*;
 
 fn nom_parser(input: &str) -> IResult<&str, PuzzleInput> {
-    todo!();
+    let (input, monkeys) = many1(parse_monkey)(input)?;
     let (input, _) = eof(input)?;
 
-    Ok((input, PuzzleInput {}))
+    Ok((input, PuzzleInput { monkeys }))
 }
 
 pub fn parse(input: &str) -> PuzzleInput {
     nom_parser(input).unwrap().1
+}
+
+pub fn parse_monkey(input: &str) -> IResult<&str, Monkey> {
+    let (input, _) = tag("Monkey ")(input)?;
+    let (input, id) = digit1(input)?;
+    let (input, _) = tag(":\n  Starting items: ")(input)?;
+    let (input, first_item) = digit1(input)?;
+    let (input, _) = opt(tag(", "))(input)?;
+    let (input, items) = opt(separated_list1(tag(", "), digit1))(input)?;
+    let (input, _) = tag("\n  Operation: new = ")(input)?;
+    let (input, operation_type) = parse_operation(input)?;
+    let (input, _) = tag("\n  Test: divisible by ")(input)?;
+    let (input, divisible_by) = digit1(input)?;
+    let (input, _) = tag("\n    If true: throw to monkey ")(input)?;
+    let (input, true_monkey) = digit1(input)?;
+    let (input, _) = tag("\n    If false: throw to monkey ")(input)?;
+    let (input, false_monkey) = digit1(input)?;
+    let (input, _) = newline(input)?;
+    let (input, _) = opt(newline)(input)?;
+
+    Ok((
+        input,
+        Monkey {
+            id: id.parse().unwrap(),
+            operation_type,
+            items: std::iter::once(vec![first_item])
+                .chain(items)
+                .flat_map(move |s| s.into_iter().map(|s| s.parse().unwrap()))
+                .collect(),
+            divisible_by: divisible_by.parse().unwrap(),
+            true_monkey: true_monkey.parse().unwrap(),
+            false_monkey: false_monkey.parse().unwrap(),
+        },
+    ))
+}
+
+pub fn parse_operation(input: &str) -> IResult<&str, OperationType> {
+    let (input, square) = opt(tag("old * old").map(|_| OperationType::Square))(input)?;
+    let (input, addition) = opt(parse_addition)(input)?;
+    let (input, multiplication) = opt(parse_multiplication)(input)?;
+
+    Ok((input, square.or(addition).or(multiplication).unwrap()))
+}
+
+pub fn parse_addition(input: &str) -> IResult<&str, OperationType> {
+    let (input, _) = tag("old + ")(input)?;
+    let (input, operand) = digit1(input)?;
+
+    Ok((input, OperationType::Add(operand.parse().unwrap())))
+}
+
+pub fn parse_multiplication(input: &str) -> IResult<&str, OperationType> {
+    let (input, _) = tag("old * ")(input)?;
+    let (input, operand) = digit1(input)?;
+
+    Ok((input, OperationType::Mul(operand.parse().unwrap())))
 }
 
 #[cfg(test)]
--- 2022/day-11/src/structs.rs
+++ 2022/day-11/src/structs.rs
@@ -2,5 +2,22 @@ use utility_belt::prelude::*;
 
 #[derive(Clone, Debug)]
 pub struct PuzzleInput {
-    // Remember to make the fields pub
+    pub monkeys: Vec<Monkey>,
+}
+
+#[derive(Clone, Debug, Copy)]
+pub enum OperationType {
+    Add(usize),
+    Square,
+    Mul(usize),
+}
+
+#[derive(Clone, Debug)]
+pub struct Monkey {
+    pub id: usize,
+    pub operation_type: OperationType,
+    pub items: Vec<usize>,
+    pub divisible_by: usize,
+    pub true_monkey: usize,
+    pub false_monkey: usize,
 }
