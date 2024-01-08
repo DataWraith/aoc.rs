3986f85dc49c5099d13208bafd260148f18c628b
Johannes Holzfuß <johannes@holzfuss.name>
2024-01-08 15:02:02 +0100

    Day 14 parser

--- 2022/day-14/src/parser.rs
+++ 2022/day-14/src/parser.rs
@@ -3,10 +3,25 @@ use utility_belt::prelude::*;
 use crate::structs::*;
 
 fn nom_parser(input: &str) -> IResult<&str, PuzzleInput> {
-    todo!();
+    let (input, segments) = many1(parse_segment)(input)?;
     let (input, _) = eof(input)?;
 
-    Ok((input, PuzzleInput {}))
+    Ok((input, PuzzleInput { segments }))
+}
+
+pub fn parse_segment(input: &str) -> IResult<&str, Vec<Coordinate>> {
+    let (input, segments) = separated_list1(tag(" -> "), parse_coordinate)(input)?;
+    let (input, _) = newline(input)?;
+
+    Ok((input, segments))
+}
+
+pub fn parse_coordinate(input: &str) -> IResult<&str, Coordinate> {
+    let (input, x) = i32(input)?;
+    let (input, _) = tag(",")(input)?;
+    let (input, y) = i32(input)?;
+
+    Ok((input, Coordinate::new(x, y)))
 }
 
 pub fn parse(input: &str) -> PuzzleInput {
