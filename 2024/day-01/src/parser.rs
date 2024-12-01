40cc472e601d5fd87057da0be881ac161873145b
Johannes Holzfuß <johannes@holzfuss.name>
2024-12-01 16:07:14 +0100

    Day 01: Slightly more elegant parsing
    This is slightly slower than before, but since the solution still runs
    in less than 1 millisecond, that is an acceptable tradeoff.

--- 2024/day-01/src/parser.rs
+++ 2024/day-01/src/parser.rs
@@ -5,8 +5,10 @@ use utility_belt::prelude::*;
 pub fn parse(input: &str) -> PuzzleInput {
     let ints = parse_ints(input);
 
-    let left = ints.iter().cloned().step_by(2).collect();
-    let right = ints.iter().cloned().skip(1).step_by(2).collect();
+    let table = Array2::from_shape_vec((ints.len() / 2, 2), ints).unwrap();
+
+    let left = table.column(0).to_vec();
+    let right = table.column(1).to_vec();
 
     PuzzleInput { left, right }
 }
