c7fe7718f335be9994b6e7969ec7f81795f29c0a
Johannes Holzfuß <johannes@holzfuss.name>
2024-12-03 13:20:22 +0100

    Day 03: Remove regex dependency

--- 2024/day-03/src/parser.rs
+++ 2024/day-03/src/parser.rs
@@ -1,35 +1,90 @@
-use regex::Regex;
-
 use crate::structs::*;
 
 pub fn parse2(input: &str) -> PuzzleInput {
-    let re = Regex::new(r"do\(\)|don\'t\(\)|mul\(\d\d?\d?,\d\d?\d?\)").unwrap();
-
-    let instructions = re.captures_iter(input).collect::<Vec<_>>();
+    let mut input = input;
+    let mut enabled = true;
     let mut muls = Vec::new();
 
-    let mut enabled = true;
-
-    for instr in instructions {
-        if &instr[0] == "do()" {
+    while !input.is_empty() {
+        if input.starts_with("do()") {
+            input = &input[4..];
             enabled = true;
-        } else if &instr[0] == "don't()" {
-            enabled = false;
-        } else if enabled {
-            let mul = parse(&instr[0]);
-            muls.push(mul.muls[0]);
+            continue;
         }
+
+        if input.starts_with("don't()") {
+            input = &input[7..];
+            enabled = false;
+            continue;
+        }
+
+        if !enabled || !input.starts_with("mul(") {
+            input = &input[1..];
+            continue;
+        }
+
+        input = &input[4..];
+
+        let Some((a, b)) = input.split_once(")") else {
+            // Unclosed parenthesis, skip the rest of the input
+            break;
+        };
+
+        if a.len() > 7 {
+            // Too many digits (also generally caused by unclosed or mismatched parentheses)
+            input = &input[1..];
+            continue;
+        }
+
+        let Some((x, y)) = a.split_once(",") else {
+            input = b;
+            continue;
+        };
+
+        let x = x.parse::<usize>().unwrap();
+        let y = y.parse::<usize>().unwrap();
+
+        muls.push(x * y);
+
+        input = b;
     }
 
     PuzzleInput { muls }
 }
 
 pub fn parse(input: &str) -> PuzzleInput {
-    let re = Regex::new(r"mul\((\d\d?\d?),(\d\d?\d?)\)").unwrap();
-    let muls = re
-        .captures_iter(input)
-        .map(|cap| cap[1].parse::<usize>().unwrap() * cap[2].parse::<usize>().unwrap())
-        .collect::<Vec<_>>();
+    let mut input = input;
+    let mut muls = Vec::new();
+
+    while !input.is_empty() {
+        if !input.starts_with("mul(") {
+            input = &input[1..];
+            continue;
+        }
+
+        input = &input[4..];
+
+        let Some((a, b)) = input.split_once(")") else {
+            break;
+        };
+
+        if a.len() > 7 {
+            input = &input[1..];
+            continue;
+        }
+
+        let Some((x, y)) = a.split_once(",") else {
+            input = b;
+            continue;
+        };
+
+        let x = x.parse::<usize>().unwrap();
+        let y = y.parse::<usize>().unwrap();
+
+        muls.push(x * y);
+
+        input = b;
+    }
 
     PuzzleInput { muls }
 }
