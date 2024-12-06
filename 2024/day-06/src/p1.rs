d4322fe76274ea69e4ff3356f98832f1e84b0b89
Johannes Holzfuß <johannes@holzfuss.name>
2024-12-06 15:37:07 +0100

    Day 06: Refactor

--- 2024/day-06/src/p1.rs
+++ 2024/day-06/src/p1.rs
@@ -6,14 +6,10 @@ use utility_belt::prelude::*;
 pub fn part1(input: &PuzzleInput) -> String {
     let coord = guard_starting_position(&input.grid);
 
-    let mut visited = HashSet::new();
-    visited.insert((coord, Direction::Up));
-
     let mut state = GuardState {
         coordinate: coord,
         direction: Direction::Up,
-        steps: 0,
-        visited,
+        visited: HashSet::new(),
     };
 
     while let Some(next_state) = state.next_state(&input.grid) {
@@ -24,19 +20,24 @@ pub fn part1(input: &PuzzleInput) -> String {
     v.sort();
     v.dedup();
 
-    v.len().to_string()
+    (v.len() + 1).to_string()
 }
 
 #[derive(Debug)]
 pub struct GuardState {
     pub coordinate: Coordinate,
     pub direction: Direction,
-    pub steps: usize,
     pub visited: HashSet<(Coordinate, Direction)>,
 }
 
 impl GuardState {
     pub fn next_state(&self, grid: &Grid2D<char>) -> Option<GuardState> {
+        if self.visited.contains(&(self.coordinate, self.direction)) {
+            return None;
+        }
+
+        let mut visited = self.visited.clone();
+
         let next_coord = self.coordinate + self.direction.into();
 
         if let Some(c) = grid.get(next_coord) {
@@ -46,18 +47,15 @@ impl GuardState {
                 return Some(GuardState {
                     coordinate: self.coordinate,
                     direction: new_direction,
-                    steps: self.steps + 1,
-                    visited: self.visited.clone(),
+                    visited,
                 });
             }
 
-            let mut visited = self.visited.clone();
-            visited.insert((next_coord, self.direction));
+            visited.insert((self.coordinate, self.direction));
 
             return Some(GuardState {
                 coordinate: next_coord,
                 direction: self.direction,
-                steps: self.steps + 1,
                 visited,
             });
         }
--- 2024/day-06/src/p2.rs
+++ 2024/day-06/src/p2.rs
@@ -9,14 +9,10 @@ use utility_belt::prelude::*;
 pub fn part2(input: &PuzzleInput) -> String {
     let coord = guard_starting_position(&input.grid);
 
-    let mut visited = HashSet::new();
-    visited.insert((coord, Direction::Up));
-
     let mut state = GuardState {
         coordinate: coord,
         direction: Direction::Up,
-        steps: 0,
-        visited,
+        visited: HashSet::new(),
     };
 
     while let Some(next_state) = state.next_state(&input.grid) {
@@ -24,34 +20,49 @@ pub fn part2(input: &PuzzleInput) -> String {
     }
 
     let mut loops_found = HashSet::new();
+    let mut obstacles = HashSet::new();
 
-    for obstacle in state.visited {
-        if obstacle.0 == coord {
+    for (pos, dir) in state.visited.iter() {
+        let obstacle = *pos + dir.clone().into();
+
+        if obstacle == coord {
             continue;
         }
 
-        let mut guard_loop = HashSet::new();
+        if input.grid.get(obstacle).is_none() {
+            continue;
+        }
+
+        if input.grid.get(obstacle).unwrap() == &'#' {
+            continue;
+        }
+
+        obstacles.insert((pos, obstacle, dir.clone()));
+    }
+
+    for (pos, obstacle, dir) in obstacles.iter() {
+        if loops_found.contains(obstacle) {
+            continue;
+        }
 
         let mut g2 = input.grid.clone();
-        g2.set(obstacle.0, '#');
+        g2.set(*obstacle, '#');
 
-        let mut visited = HashSet::new();
-        visited.insert((coord, Direction::Up));
+        let start = *pos;
+        let start_dir = *dir;
 
         let mut state = GuardState {
-            coordinate: coord,
-            direction: Direction::Up,
-            steps: 0,
-            visited,
+            coordinate: *start,
+            direction: start_dir,
+            visited: HashSet::new(),
         };
 
         while let Some(next_state) = state.next_state(&g2) {
-            if !guard_loop.insert((next_state.coordinate, next_state.direction)) {
-                loops_found.insert(obstacle.0);
-                break;
-            }
-
             state = next_state;
+        }
+
+        if g2.get(state.coordinate + state.direction.into()).is_some() {
+            loops_found.insert(obstacle);
         }
     }
 
