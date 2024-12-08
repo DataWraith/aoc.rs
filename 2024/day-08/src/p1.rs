b9161a09add5d60b750f83185622a2ee15a4e1ae
Johannes Holzfuß <johannes@holzfuss.name>
2024-12-08 06:54:48 +0100

    Day 08: Cleanup

--- 2024/day-08/src/p1.rs
+++ 2024/day-08/src/p1.rs
@@ -4,68 +4,53 @@ use utility_belt::prelude::*;
 
 #[tracing::instrument(skip(input))]
 pub fn part1(input: &PuzzleInput) -> String {
-    let antennas = frequencies(&input.grid);
+    let freqs = frequencies(&input.grid);
     let mut total = HashSet::new();
 
-    for frequency in antennas {
+    for frequency in freqs {
         let coordinates = antenna_coordinates(&input.grid, frequency);
-        let antipodes = antipodes(
+        let antinodes = all_antinodes(
             input.grid.width() as i32,
             input.grid.height() as i32,
             &coordinates,
         );
 
-        total.extend(antipodes);
+        total.extend(antinodes);
     }
-
-    let mut dbg_grid = Grid2D::new(input.grid.width(), input.grid.height(), '.');
-
-    for t in total.iter() {
-        println!("{}", t);
-        dbg_grid.set(*t, '#');
-    }
-
-    for (c, a) in input.grid.iter() {
-        if *a != '.' && *a != '#' {
-            dbg_grid.set(c.clone(), *a);
-        }
-    }
-
-    println!("{}", &dbg_grid);
 
     total.len().to_string()
 }
 
 pub fn frequencies(grid: &Grid2D<char>) -> HashSet<char> {
     grid.iter()
-        .filter(|(_, c)| **c != '.')
-        .map(|(_, c)| c.clone())
+        .filter(|(_, c)| **c != '.' && **c != '#')
+        .map(|(_, c)| *c)
         .collect()
 }
 
 pub fn antenna_coordinates(grid: &Grid2D<char>, antenna: char) -> HashSet<Coordinate> {
     grid.iter()
         .filter(|(_, c)| **c == antenna)
-        .map(|(coord, _)| coord.clone())
+        .map(|(coord, _)| coord)
         .collect()
 }
 
-pub fn antipode(a: &Coordinate, b: &Coordinate) -> [Coordinate; 2] {
+pub fn antinodes(a: &Coordinate, b: &Coordinate) -> [Coordinate; 2] {
     let dx = a.x - b.x;
     let dy = a.y - b.y;
 
     [
-        Coordinate::new(a.x + dx as i32, a.y + dy as i32),
-        Coordinate::new(b.x - dx as i32, b.y - dy as i32),
+        Coordinate::new(a.x + dx, a.y + dy),
+        Coordinate::new(b.x - dx, b.y - dy),
     ]
 }
 
-pub fn antipodes(w: i32, h: i32, coordinates: &HashSet<Coordinate>) -> HashSet<Coordinate> {
+pub fn all_antinodes(w: i32, h: i32, coordinates: &HashSet<Coordinate>) -> HashSet<Coordinate> {
     coordinates
         .iter()
         .cloned()
         .combinations(2)
-        .flat_map(|x| antipode(&x[0], &x[1]))
+        .flat_map(|x| antinodes(&x[0], &x[1]))
         .filter(|c| c.x >= 0 && c.x < w && c.y >= 0 && c.y < h)
         .collect()
 }
@@ -73,22 +58,22 @@ pub fn antipodes(w: i32, h: i32, coordinates: &HashSet<Coordinate>) -> HashSet<C
 #[cfg(test)]
 mod tests {
     use super::*;
-    use utility_belt::prelude::*;
+
 
     const TEST_INPUT: &str = indoc! {"
-............
-........0...
-.....0......
-.......0....
-....0.......
-......A.....
-............
-............
-........A...
-.........A..
-............
-............
-"};
+        ............
+        ........0...
+        .....0......
+        .......0....
+        ....0.......
+        ......A.....
+        ............
+        ............
+        ........A...
+        .........A..
+        ............
+        ............
+    "};
 
     #[test]
     fn test_part1_example() {
--- 2024/day-08/src/p2.rs
+++ 2024/day-08/src/p2.rs
@@ -7,72 +7,56 @@ use utility_belt::prelude::*;
 
 #[tracing::instrument(skip(input))]
 pub fn part2(input: &PuzzleInput) -> String {
-    let antennas = frequencies(&input.grid);
+    let freqs = frequencies(&input.grid);
     let mut total = HashSet::new();
 
-    for frequency in antennas {
+    for frequency in freqs {
         let coordinates = antenna_coordinates(&input.grid, frequency);
-        let antipodes = antipodes(
+        let antinodes = all_antinodes(
             input.grid.width() as i32,
             input.grid.height() as i32,
             &coordinates,
         );
 
-        total.extend(antipodes);
+        total.extend(antinodes);
     }
-
-    let mut dbg_grid = Grid2D::new(input.grid.width(), input.grid.height(), '.');
-
-    for t in total.iter() {
-        println!("{}", t);
-        dbg_grid.set(*t, '#');
-    }
-
-    for (c, a) in input.grid.iter() {
-        if *a != '.' && *a != '#' {
-            dbg_grid.set(c.clone(), *a);
-        }
-    }
-
-    println!("{}", &dbg_grid);
 
     total.len().to_string()
 }
 
-pub fn antipodes(w: i32, h: i32, coordinates: &HashSet<Coordinate>) -> HashSet<Coordinate> {
+pub fn all_antinodes(w: i32, h: i32, coordinates: &HashSet<Coordinate>) -> HashSet<Coordinate> {
     coordinates
         .iter()
         .cloned()
         .combinations(2)
-        .flat_map(|x| antipode(&x[0], &x[1], w, h))
+        .flat_map(|x| antinodes(&x[0], &x[1], w, h))
         .collect()
 }
 
-pub fn antipode(a: &Coordinate, b: &Coordinate, width: i32, height: i32) -> Vec<Coordinate> {
+pub fn antinodes(a: &Coordinate, b: &Coordinate, width: i32, height: i32) -> Vec<Coordinate> {
     let dx = a.x - b.x;
     let dy = a.y - b.y;
 
     let mut result = Vec::new();
 
-    result.push(a.clone());
-    result.push(b.clone());
+    for i in 0.. {
+        let c = Coordinate::new(a.x + dx * i, a.y + dy * i);
 
-    for i in 1.. {
-        let c = Coordinate::new(a.x + dx * i as i32, a.y + dy * i as i32);
-        if c.x >= 0 && c.x < width && c.y >= 0 && c.y < height {
-            result.push(c);
-        } else {
+        if c.x < 0 || c.x >= width || c.y < 0 || c.y >= height {
             break;
         }
+
+        result.push(c);
     }
 
-    for i in 1.. {
-        let c = Coordinate::new(b.x - dx * i as i32, b.y - dy * i as i32);
-        if c.x >= 0 && c.x < width && c.y >= 0 && c.y < height {
-            result.push(c);
-        } else {
+    for i in 0.. {
+        let c = Coordinate::new(b.x - dx * i, b.y - dy * i);
+
+        if c.x < 0 || c.x >= width || c.y < 0 || c.y >= height {
             break;
         }
+
+        result.push(c);
     }
 
     result
@@ -81,24 +65,26 @@ pub fn antipode(a: &Coordinate, b: &Coordinate, width: i32, height: i32) -> Vec<
 #[cfg(test)]
 mod tests {
     use super::*;
-    use utility_belt::prelude::*;
+
 
     const TEST_INPUT: &str = indoc! {"
-T.........
-...T......
-.T........
-..........
-..........
-..........
-..........
-..........
-..........
-..........
-"};
+        ............
+        ........0...
+        .....0......
+        .......0....
+        ....0.......
+        ......A.....
+        ............
+        ............
+        ........A...
+        .........A..
+        ............
+        ............
+    "};
 
     #[test]
     fn test_part2_example() {
         let input = crate::parser::part2(TEST_INPUT);
-        assert_eq!(part2(&input), "9");
+        assert_eq!(part2(&input), "34");
     }
 }
