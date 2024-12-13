a24b9a180dc212e7e01e85a13b15535a28c28f25
Johannes Holzfuß <johannes@holzfuss.name>
2024-12-13 06:38:27 +0100

    Part 1

--- 2024/day-13/src/p1.rs
+++ 2024/day-13/src/p1.rs
@@ -1,10 +1,64 @@
+use cached::proc_macro::cached;
 use utility_belt::prelude::*;
 
 use crate::parser::*;
 
 #[tracing::instrument(skip(input))]
 pub fn part1(input: &PuzzleInput) -> String {
-    todo!("day_13::p1::part1");
+    let result = input
+        .games
+        .iter()
+        .filter_map(|g| calculate_num_button_presses(g.clone()))
+        .collect_vec();
+    result
+        .iter()
+        .map(|(a, b)| 3 * a + b)
+        .sum::<usize>()
+        .to_string()
+}
+
+#[cached]
+fn calculate_num_button_presses(claw_game: ClawGame) -> Option<(usize, usize)> {
+    if claw_game.prize == Coordinate::new(0, 0) {
+        return Some((0, 0));
+    }
+
+    let mut min = usize::MAX;
+    let mut result = None;
+
+    if claw_game.prize >= claw_game.offset_a {
+        let a_game = ClawGame {
+            prize: claw_game.prize - claw_game.offset_a,
+            ..claw_game
+        };
+
+        if let Some(m) = calculate_num_button_presses(a_game).map(|(a, b)| (a + 1, b)) {
+            let c = m.0 * 3 + m.1;
+
+            if c < min {
+                min = c;
+                result = Some(m);
+            }
+        }
+    }
+
+    if claw_game.prize >= claw_game.offset_b {
+        let b_game = ClawGame {
+            prize: claw_game.prize - claw_game.offset_b,
+            ..claw_game
+        };
+
+        if let Some(m) = calculate_num_button_presses(b_game).map(|(a, b)| (a, b + 1)) {
+            let c = m.0 * 3 + m.1;
+
+            if c < min {
+                min = c;
+                result = Some(m);
+            }
+        }
+    }
+
+    result
 }
 
 #[cfg(test)]
@@ -13,13 +67,27 @@ mod tests {
     use utility_belt::prelude::*;
 
     const TEST_INPUT: &str = indoc! {"
-        TODO
-    "};
+Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279
+"};
 
     #[test]
     fn test_part1_example() {
         let input = crate::parser::part1(TEST_INPUT);
         assert_ne!(TEST_INPUT, "TODO");
-        assert_eq!(part1(&input), "TODO");
+        assert_eq!(part1(&input), "480");
     }
 }
--- 2024/day-13/src/parser.rs
+++ 2024/day-13/src/parser.rs
@@ -1,41 +1,57 @@
-use winnow::{PResult, Parser, combinator::trace};
+use regex::Regex;
+use utility_belt::prelude::*;
 
 #[derive(Clone, Debug)]
 pub struct PuzzleInput {
     // Remember to make the fields pub
+    pub games: Vec<ClawGame>,
 }
 
-fn winnow_parser<'s>(input: &mut &'s str) -> PResult<PuzzleInput> {
-    Ok(PuzzleInput{})
+#[derive(Clone, Debug, Hash, PartialEq, Eq)]
+pub struct ClawGame {
+    pub offset_a: Coordinate,
+    pub offset_b: Coordinate,
+    pub prize: Coordinate,
 }
 
 pub fn part1(input: &str) -> PuzzleInput {
-    // Checklist:
-    //
-    // 1. Can this be parsed using parse_ints(input)
-    // 2. Can this be parsed using a regular expression
-    // 3. Winnow parser?
-    // 4. ???
+    let mut result = Vec::new();
 
-    winnow_parser.parse(input).unwrap()
+    let games = input.split("\n\n");
+
+    for g in games.into_iter() {
+        let lines = g.lines().collect::<Vec<_>>();
+
+        let re = Regex::new(r"X\+(\d+), Y\+(\d+)").unwrap();
+
+        let mut offset_a = Coordinate::new(0, 0);
+        let mut offset_b = Coordinate::new(0, 0);
+        let mut prize = Coordinate::new(0, 0);
+
+        for (_, [x, y]) in re.captures_iter(lines[0]).map(|c| c.extract()) {
+            offset_a = Coordinate::new(x.parse::<i32>().unwrap(), y.parse::<i32>().unwrap());
+        }
+
+        for (_, [x, y]) in re.captures_iter(lines[1]).map(|c| c.extract()) {
+            offset_b = Coordinate::new(x.parse::<i32>().unwrap(), y.parse::<i32>().unwrap());
+        }
+
+        let re = Regex::new(r"X=(\d+), Y=(\d+)").unwrap();
+
+        for (_, [x, y]) in re.captures_iter(lines[2]).map(|c| c.extract()) {
+            prize = Coordinate::new(x.parse::<i32>().unwrap(), y.parse::<i32>().unwrap());
+        }
+
+        result.push(ClawGame {
+            offset_a,
+            offset_b,
+            prize,
+        });
+    }
+
+    PuzzleInput { games: result }
 }
 
 pub fn part2(input: &str) -> PuzzleInput {
     part1(input)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use utility_belt::prelude::*;
-
-    const TEST_INPUT: &str = indoc! {"
-        TODO
-    "};
-
-    #[test]
-    fn test_parse() {
-        assert_ne!(TEST_INPUT, "TODO");
-        assert!(trace("Puzzle", winnow_parser).parse_next(&mut TEST_INPUT.clone()).is_ok());
-    }
 }
