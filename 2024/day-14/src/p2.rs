181d71eed95498af51704a048760c04f79f20889
Johannes Holzfuß <johannes@holzfuss.name>
2024-12-14 09:44:49 +0100

    Day 14: Part 2: More speed opt

--- 2024/day-14/src/p2.rs
+++ 2024/day-14/src/p2.rs
@@ -6,7 +6,7 @@ use crate::parser::*;
 pub fn part2(input: &PuzzleInput) -> String {
     let mut trajectories = input.robots.clone();
 
-    for i in 1.. {
+    'outer: for i in 1.. {
         for robot in trajectories.iter_mut() {
             robot.step();
         }
@@ -16,6 +16,17 @@ pub fn part2(input: &PuzzleInput) -> String {
         let mut grid = Grid2D::new(101, 103, false);
 
         for robot in trajectories.iter() {
+            // If you think about how the puzzle might have been designed, the
+            // initial state of all robots was probably unique and then
+            // perturbed by the inverse velocities.
+            //
+            // So in the initial picture showing the christmas tree, all robots
+            // would have been in unique positions. So we discard any state
+            // where two robots overlap.
+            if grid.get(robot.position) == Some(&true) {
+                continue 'outer;
+            }
+
             grid.set(robot.position, true);
         }
 
