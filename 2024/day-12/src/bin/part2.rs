d295828ab9c6769bdfb39063356b660896f217f4
Johannes Holzfuß <johannes@holzfuss.name>
2024-12-18 13:35:16 +0100

    Day 18: More elegant solution for part 2

--- 2024/day-12/src/bin/part2.rs
+++ 2024/day-12/src/bin/part2.rs
@@ -9,8 +9,6 @@ mod tests {
     #[test]
     fn test_part2() {
         let puzzle_input = day_12::parser::part2(include_str!("../../input.txt"));
-        // 847472
-        // 839842
         assert_eq!(day_12::p2::part2(&puzzle_input), "844132");
     }
 }
--- 2024/day-18/src/p2.rs
+++ 2024/day-18/src/p2.rs
@@ -1,22 +1,67 @@
 use utility_belt::prelude::*;
 
-use crate::{p1::breadth_first_search, parser::*};
+use crate::parser::*;
 
 pub fn part2(input: &PuzzleInput) -> String {
-    let idx = bisect(0, input.bytes.len(), |i| {
-        let mut memory = Grid2D::new(input.width, input.height, '.');
+    // This solution uses UnionFind again. I thought of trying this myself, but
+    // concluded that it wouldn't work -- and it doesn't in the forward
+    // direction. But I later found out on Reddit that you can just reverse
+    // everything, and it works marvelously.
 
-        for b in input.bytes.iter().take(*i) {
-            memory.set(*b, '#');
+    // Step 1: Find all obstacle coordinates and make a map
+    let mut grid = Grid2D::new(input.width, input.height, '.');
+
+    for b in input.bytes.iter() {
+        grid.set(*b, '#');
+    }
+
+    // Step 2: Prepare the UnionFind datastructure
+    let mut union_find = UnionFind::default();
+    let mut sets = grid.map(|_| 0);
+
+    for (c, _) in grid.iter() {
+        sets[c] = union_find.make_set();
+    }
+
+    // Step 3: Union all coordinates that never have obstacles
+    for (c, _) in grid.iter() {
+        if grid.get(c) != Some(&'.') {
+            continue;
         }
 
-        let cost = breadth_first_search(&memory);
+        for dir in [Direction::Right, Direction::Down] {
+            let n = c.neighbor(dir);
 
-        cost.is_none()
-    })
-    .unwrap();
+            if grid.get(n) == Some(&'.') {
+                union_find.union(sets[c], sets[n]).unwrap();
+            }
+        }
+    }
 
-    format!("{},{}", input.bytes[idx].x, input.bytes[idx].y)
+    // Step 4: Iterate over the obstacles in reverse, and union the coordinates.
+    // This is the non-obvious part. Instead of adding obstacles one by one,
+    // we start with all obstacles and then remove them one by one, unioning
+    // the free space.
+    let start = Coordinate::new(0, 0);
+    let end = Coordinate::new(input.width as i32 - 1, input.height as i32 - 1);
+
+    for b in input.bytes.iter().rev() {
+        grid.set(*b, '.');
+
+        for n in b.neighbors() {
+            if grid.get(n) == Some(&'.') {
+                union_find.union(sets[*b], sets[n]).unwrap();
+            }
+        }
+
+        // Step 5: If the start and end coordinates are connected, the last
+        // obstacle we removed must have been the one blocking the path.
+        if union_find.find(sets[start]) == union_find.find(sets[end]) {
+            return format!("{},{}", b.x, b.y);
+        }
+    }
+
+    unreachable!()
 }
 
 #[cfg(test)]
