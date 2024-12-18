bb1fc6425417c3a3a867535ad73303cf8fb2bd30
Johannes Holzfuß <johannes@holzfuss.name>
2024-12-18 10:34:03 +0100

    WIP

--- 2024/day-18/src/p2.rs
+++ 2024/day-18/src/p2.rs
@@ -4,39 +4,6 @@ use utility_belt::prelude::*;
 
 use crate::parser::*;
 
-pub fn shortest_path_exists(grid: &Grid2D<i32>, start: Coordinate) -> Option<Grid2D<i32>> {
-    let mut memory = grid.clone();
-
-    let mut q = VecDeque::new();
-
-    q.push_back((
-        Coordinate::new(grid.width as i32 - 1, grid.height as i32 - 1),
-        1,
-    ));
-
-    memory.set(
-        Coordinate::new(grid.width as i32 - 1, grid.height as i32 - 1),
-        1,
-    );
-
-    while let Some((c, cost)) = q.pop_front() {
-        if c == start {
-            return Some(memory);
-        }
-
-        for d in Direction::cardinal() {
-            let nc = c + d;
-
-            if memory.get(nc) < Some(&(cost + 1)) {
-                memory.set(nc, cost + 1);
-                q.push_back((nc, cost + 1));
-            }
-        }
-    }
-
-    None
-}
-
 pub fn part2(input: &PuzzleInput) -> String {
     let mut memory = Grid2D::new(input.width, input.height, 0);
 
@@ -76,7 +43,10 @@ pub fn part2(input: &PuzzleInput) -> String {
             return format!("{},{}", byte_coord.x, byte_coord.y);
         }
 
+        let prev_cost = *memory.get(*byte_coord).unwrap();
         memory.set(*byte_coord, max_val);
+
+        q.clear();
 
         for d in Direction::cardinal() {
             let nc = *byte_coord + d;
@@ -89,18 +59,7 @@ pub fn part2(input: &PuzzleInput) -> String {
                 continue;
             }
 
-            for nc2 in nc.neighbors() {
-                if memory.get(nc2).is_none() {
-                    continue;
-                }
-
-                let cost = *memory.get(nc).unwrap();
-                let neighbor_cost = *memory.get(nc2).unwrap();
-
-                if neighbor_cost < max_val {
-                    memory.set(nc, (neighbor_cost + 1).min(cost));
-                }
-            }
+            q.push_back((nc, prev_cost + 1));
         }
 
         dbg!(&memory.map(|x| if *x == 999 {
@@ -110,179 +69,61 @@ pub fn part2(input: &PuzzleInput) -> String {
         }));
         dbg!(&byte_coord);
 
-        let mut q = VecDeque::new();
-        q.push_back(Coordinate::new(0, 0));
+        let mut found = false;
+        let mut visited = memory.map(|_| max_val);
 
-        while let Some(cur) = q.pop_front() {
-            if cur == Coordinate::new(input.width as i32 - 1, input.height as i32 - 1) {
-                continue 'outer;
-            }
-
+        while let Some((cur, _cost)) = q.pop_front() {
             let cost = *memory.get(cur).unwrap();
 
-            for d in Direction::cardinal() {
-                let nc = cur + d;
+            if cur == Coordinate::new(input.width as i32 - 1, input.height as i32 - 1) {
+                found = true;
+                continue;
+            }
 
-                if memory.get(nc).is_none() {
+            if *visited.get(cur).unwrap() == cost {
+                continue;
+            }
+
+            visited.set(cur, cost);
+
+            if cost == max_val {
+                continue;
+            }
+
+            let mut min_cost = max_val;
+
+            for n in cur.neighbors() {
+                if memory.get(n).is_none() {
                     continue;
                 }
 
-                let neighbor_cost = *memory.get(nc).unwrap();
+                min_cost = min_cost.min(1 + *memory.get(n).unwrap());
+            }
 
-                dbg!(&neighbor_cost);
-                dbg!(&cost);
+            if min_cost == max_val {
+                memory.set(cur, max_val);
+            } else if *memory.get(cur).unwrap() != min_cost + 1 {
+                memory.set(cur, min_cost);
+                q.push_back((cur, min_cost));
+            }
 
-                if neighbor_cost + 1 == cost {
-                    q.push_back(nc);
+            if min_cost == prev_cost {
+                found = true;
+                continue;
+            }
+
+            for n in cur.neighbors() {
+                if memory.get(n).is_none() {
+                    continue;
                 }
+
+                q.push_back((n, 0)); //(min_cost + 1).min(*neighbor_cost)));
             }
         }
 
-        //let mut visisted = memory.map(|x| x + 1);
-        //visisted.set(
-        //Coordinate::new(input.width as i32 - 1, input.height as i32 - 1),
-        //1,
-        //);
-
-        //while let Some((c, cost, origin)) = q2.pop_front() {
-        ////dbg!(&memory.map(|x| if *x == 150 {
-        ////"###".to_string()
-        ////} else {
-        ////format!("{:03}", x)
-        ////}));
-        ////dbg!(&byte_coord);
-
-        //if visisted.get(c) == Some(&cost) {
-        //continue;
-        //}
-
-        //visisted.set(c, cost);
-
-        //let mut min_neighbor_cost = max_val;
-
-        //for d in Direction::cardinal() {
-        //let nc = c + d;
-
-        //if memory.get(nc).is_none() {
-        //continue;
-        //}
-
-        //let neighbor_cost = memory.get(nc).unwrap();
-        //min_neighbor_cost = min_neighbor_cost.min(*neighbor_cost);
-        //}
-
-        //if min_neighbor_cost >= max_val {
-        //memory.set(c, max_val);
-        //continue;
-        //}
-
-        //memory.set(c, min_neighbor_cost + 1);
-
-        //for d in Direction::cardinal() {
-        //let nc = c + d;
-
-        //if nc == origin {
-        //continue;
-        //}
-
-        //if memory.get(nc).is_none() {
-        //continue;
-        //}
-
-        //let neighbor_cost = memory.get(nc).unwrap();
-
-        //if *neighbor_cost == min_neighbor_cost {
-        //q2.push_back((nc, *neighbor_cost, c));
-        //}
-        //}
-
-        ////if c == Coordinate::new(input.width as i32 - 1, input.height as i32 - 1) {
-        ////found_end.insert(origin);
-        ////continue;
-        ////}
-
-        ////if c == Coordinate::new(0, 0) {
-        ////found_start.insert(origin);
-        ////}
-
-        ////if *memory.get(c).unwrap() == max_val {
-        ////continue;
-        ////}
-
-        ////if *visisted.get(c).unwrap() == cost {
-        ////continue;
-        ////}
-
-        ////visisted.set(c, cost);
-
-        ////let mut min_cost = max_val;
-
-        ////for d in Direction::cardinal() {
-        ////let nc = c + d;
-
-        ////if memory.get(nc).is_some() {
-        ////let ncost = memory.get(nc).unwrap();
-        ////min_cost = min_cost.min(ncost + 1);
-        ////}
-        ////}
-
-        ////let min_cost_changed = min_cost != cost;
-
-        ////if min_cost < max_val && min_cost >= cost {
-        ////for d in Direction::cardinal() {
-        ////let nc = c + d;
-
-        ////if memory.get(nc).is_some() {
-        ////if *memory.get(nc).unwrap() != max_val {
-        ////q2.push_back((nc, *memory.get(nc).unwrap(), origin));
-        ////}
-        ////}
-        ////}
-        ////}
-
-        ////memory.set(c, min_cost);
-        //}
-
-        ////if found_start.union(&found_end).count() == 0 {
-        ////    dbg!(&memory.map(|x| if *x == 150 {
-        ////        "###".to_string()
-        ////    } else {
-        ////        format!("{:03}", x)
-        ////    }));
-        ////    dbg!(&byte_coord);
-        ////}
-
-        ////dbg!("Finding path");
-
-        //q.clear();
-        //q.push_back((
-        //Coordinate::new(0, 0),
-        //*memory.get(Coordinate::new(0, 0)).unwrap(),
-        //));
-
-        //while let Some((c, cost)) = q.pop_front() {
-        //for d in Direction::cardinal() {
-        //let nc = c + d;
-
-        //if nc == Coordinate::new(input.width as i32 - 1, input.height as i32 - 1) {
-        //q.clear();
-        //continue 'outer;
-        //}
-
-        //if memory.get(nc) == Some(&(cost - 1)) {
-        //q.push_back((nc, cost - 1));
-        //}
-        //}
-        //}
-
-        dbg!(&memory.map(|x| if *x == 999 {
-            "###".to_string()
-        } else {
-            format!("{:03}", x)
-        }));
-        dbg!(&byte_coord);
-
-        return format!("{},{}", byte_coord.x, byte_coord.y);
+        if !found {
+            return format!("{},{}", byte_coord.x, byte_coord.y);
+        }
     }
 
     unreachable!()
