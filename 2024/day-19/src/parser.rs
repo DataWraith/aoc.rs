e3e67ccf323ca3d03dd59d65cdef113b74815494
Johannes Holzfuß <johannes@holzfuss.name>
2024-12-19 09:18:27 +0100

    Day 19: Remove unnecessary sorting of patterns

--- 2024/day-19/src/parser.rs
+++ 2024/day-19/src/parser.rs
@@ -6,15 +6,12 @@ pub struct PuzzleInput {
 
 pub fn part1(input: &str) -> PuzzleInput {
     let (patterns, desired_designs) = input.split_once("\n\n").unwrap();
-    let mut patterns: Vec<String> = patterns.split(", ").map(|s| s.to_string()).collect();
+    let patterns: Vec<String> = patterns.split(", ").map(|s| s.to_string()).collect();
     let designs: Vec<String> = desired_designs
         .trim_end()
         .split("\n")
         .map(|s| s.to_string())
         .collect();
-
-    patterns.sort_by_key(|p| p.len());
-    patterns.reverse();
 
     PuzzleInput {
         patterns,
@@ -25,21 +22,3 @@ pub fn part1(input: &str) -> PuzzleInput {
 pub fn part2(input: &str) -> PuzzleInput {
     part1(input)
 }
-
-/*
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use utility_belt::prelude::*;
-
-    const TEST_INPUT: &str = indoc! {"
-        TODO
-    "};
-
-    #[test]
-    fn test_parse() {
-        assert_ne!(TEST_INPUT, "TODO");
-        assert!(trace("Puzzle", winnow_parser).parse_next(&mut TEST_INPUT.clone()).is_ok());
-    }
-}
-*/
