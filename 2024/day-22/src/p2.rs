f377cfa1e9f2849d86aa164508746415bc66bbe8
Johannes Holzfuß <johannes@holzfuss.name>
2024-12-23 08:08:11 +0100

    Day 22: Refactor p2

--- 2024/day-22/src/p2.rs
+++ 2024/day-22/src/p2.rs
@@ -3,40 +3,31 @@ use utility_belt::prelude::*;
 use crate::{p1::hash, parser::*};
 
 pub fn part2(input: &PuzzleInput) -> String {
-    let mut scores = HashMap::new();
+    let mut potential_earnings = HashMap::new();
 
     for seed in input.numbers.iter() {
         let prices = prices(*seed);
         let differences = price_differences(&prices);
 
-        let seq_map = find_sequences(&differences, &prices);
+        let seq_map = sequences_and_earnings(&differences, &prices);
 
         for (seq, bananas) in seq_map.iter() {
-            scores
+            potential_earnings
                 .entry(*seq)
                 .and_modify(|b| *b += *bananas as u64)
                 .or_insert(*bananas as u64);
         }
     }
 
-    scores.values().max().unwrap().to_string()
+    potential_earnings.values().max().unwrap().to_string()
 }
 
-pub fn earnings(sequence: [i8; 4], prices: &[u8], differences: &[i8]) -> u64 {
-    let windows = differences.windows(4).skip(1);
-
-    for (i, window) in windows.enumerate() {
-        if window == sequence {
-            return prices[i + 4] as u64;
-        }
-    }
-
-    0
-}
-
-pub fn find_sequences(differences: &[i8], prices: &[u8]) -> HashMap<[i8; 4], u8> {
+// Calculates the potential earnings for each sequence of 4 price differences in
+// the data.
+pub fn sequences_and_earnings(differences: &[i8], prices: &[u8]) -> HashMap<[i8; 4], u8> {
     differences
         .windows(4)
+        // We need to skip the first window, because there is no difference before the first price
         .skip(1)
         .enumerate()
         .fold(HashMap::new(), |mut map, (i, w)| {
@@ -44,6 +35,8 @@ pub fn find_sequences(differences: &[i8], prices: &[u8]) -> HashMap<[i8; 4], u8>
             seq.copy_from_slice(w);
 
             if !map.contains_key(&seq) {
+                // Since windows() goes forward in time instead of backward, we
+                // need to add 4 to the index to get the correct price
                 map.insert(seq, prices[i + 4]);
             }
 
@@ -51,26 +44,21 @@ pub fn find_sequences(differences: &[i8], prices: &[u8]) -> HashMap<[i8; 4], u8>
         })
 }
 
+// Calculates the prices for a given seed.
 pub fn prices(seed: u64) -> Vec<u8> {
-    let mut prices = Vec::new();
-    let mut seed = seed;
-
-    while prices.len() < 2000 {
-        prices.push((seed % 10) as u8);
-        seed = hash(seed);
-    }
-
-    prices
+    std::iter::successors(Some(seed), |&seed| Some(hash(seed)))
+        .take(2000)
+        .map(|x| (x % 10) as u8)
+        .collect()
 }
 
+// Calculates the price differences for a given sequence of prices. Note that
+// the difference sequence must start with an arbitrary padding value to line up
+// the indices with the prices.
 pub fn price_differences(prices: &[u8]) -> Vec<i8> {
-    let mut differences = vec![0];
-
-    for i in 1..prices.len() {
-        differences.push(prices[i] as i8 - prices[i - 1] as i8);
-    }
-
-    differences
+    std::iter::once(0)
+        .chain(prices.windows(2).map(|w| w[1] as i8 - w[0] as i8))
+        .collect()
 }
 
 #[cfg(test)]
