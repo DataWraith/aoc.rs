be733adf383fe119c43e9ffd71b0db3b4b4a46bb
Johannes Holzfuß <johannes@holzfuss.name>
2024-12-23 07:42:58 +0100

    Day 22: Part 2 (brute force)

--- 2024/day-22/src/bin/part2.rs
+++ 2024/day-22/src/bin/part2.rs
@@ -8,6 +8,7 @@ mod tests {
     #[test]
     fn test_part2() {
         let puzzle_input = day_22::parser::part2(include_str!("../../input.txt"));
+        // 16758 is too high
         assert_eq!(day_22::p2::part2(&puzzle_input), "TODO");
     }
 }
--- 2024/day-22/src/p2.rs
+++ 2024/day-22/src/p2.rs
@@ -1,9 +1,87 @@
+use rayon::prelude::*;
+
 use utility_belt::prelude::*;
 
-use crate::parser::*;
+use crate::{p1::hash, parser::*};
 
 pub fn part2(input: &PuzzleInput) -> String {
-    todo!("day_22::p2::part2");
+    let mut sequences = HashSet::new();
+
+    let mut prices_vec = Vec::new();
+    let mut differences_vec = Vec::new();
+
+    for seed in input.numbers.iter() {
+        let prices = prices(*seed);
+        let differences = price_differences(&prices);
+
+        sequences.extend(find_sequences(&differences));
+
+        prices_vec.push(prices);
+        differences_vec.push(differences);
+    }
+
+    let mut best_earnings = 0;
+
+    for (i, sequence) in sequences.iter().enumerate() {
+        dbg!(i, sequences.len());
+
+        let mut bananas = 0;
+
+        for (j, seed) in input.numbers.iter().enumerate() {
+            bananas += earnings(*sequence, &prices_vec[j], &differences_vec[j]);
+        }
+
+        if bananas > best_earnings {
+            best_earnings = bananas;
+        }
+    }
+
+    best_earnings.to_string()
+}
+
+pub fn earnings(sequence: [i8; 4], prices: &[u8], differences: &[i8]) -> u64 {
+    let windows = differences.windows(4).skip(1);
+
+    for (i, window) in windows.enumerate() {
+        if window == sequence {
+            return prices[i + 4] as u64;
+        }
+    }
+
+    0
+}
+
+pub fn find_sequences(differences: &[i8]) -> HashSet<[i8; 4]> {
+    differences
+        .windows(4)
+        .map(|w| {
+            let mut seq = [0; 4];
+            seq.copy_from_slice(w);
+            seq
+        })
+        .collect()
+}
+
+pub fn prices(seed: u64) -> Vec<u8> {
+    let mut prices = Vec::new();
+    let mut seed = seed;
+
+    while prices.len() < 2000 {
+        prices.push((seed % 10) as u8);
+        seed = hash(seed);
+    }
+
+    prices
+}
+
+pub fn price_differences(prices: &[u8]) -> Vec<i8> {
+    let mut differences = vec![0];
+
+    for i in 1..prices.len() {
+        differences.push(prices[i] as i8 - prices[i - 1] as i8);
+    }
+
+    differences
 }
 
 #[cfg(test)]
@@ -12,13 +90,16 @@ mod tests {
     use utility_belt::prelude::*;
 
     const TEST_INPUT: &str = indoc! {"
-        TODO
-    "};
+1
+2
+3
+2024
+"};
 
     #[test]
     fn test_part2_example() {
         let input = crate::parser::part2(TEST_INPUT);
         assert_ne!(TEST_INPUT, "TODO\n");
-        assert_eq!(part2(&input), "TODO");
+        assert_eq!(part2(&input), "23");
     }
 }
