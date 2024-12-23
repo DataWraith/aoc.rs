a999b88e6bd82442e70f4d78a6976867ab535e73
Johannes Holzfuß <johannes@holzfuss.name>
2024-12-23 13:30:22 +0100

    Day 21: Fill in solution values

--- 2024/day-21/src/bin/part1.rs
+++ 2024/day-21/src/bin/part1.rs
@@ -8,8 +8,6 @@ mod tests {
     #[test]
     fn test_part1() {
         let puzzle_input = day_21::parser::part1(include_str!("../../input.txt"));
-        // Too high: 223770
-        // Too high: 218302
-        assert_eq!(day_21::p1::part1(&puzzle_input), "TODO");
+        assert_eq!(day_21::p1::part1(&puzzle_input), "211930");
     }
 }
--- 2024/day-21/src/bin/part2.rs
+++ 2024/day-21/src/bin/part2.rs
@@ -8,6 +8,6 @@ mod tests {
     #[test]
     fn test_part2() {
         let puzzle_input = day_21::parser::part2(include_str!("../../input.txt"));
-        assert_eq!(day_21::p2::part2(&puzzle_input), "TODO");
+        assert_eq!(day_21::p2::part2(&puzzle_input), "263492840501566");
     }
 }
