6bf7b4f2a528476d485c899f1ec419971ecaaab7
Johannes Holzfuß <johannes@holzfuss.name>
2024-12-23 12:37:24 +0100

    Day 22: Refactor

--- 2024/day-22/src/p2.rs
+++ 2024/day-22/src/p2.rs
@@ -7,58 +7,30 @@ pub fn part2(input: &PuzzleInput) -> String {
 
     for seed in input.numbers.iter() {
         let prices = prices(*seed);
-        let differences = price_differences(&prices);
+        let mut seen = HashSet::new();
 
-        let seq_map = sequences_and_earnings(&differences, &prices);
+        prices.tuple_windows().for_each(|(a, b, c, d, e)| {
+            let seq = [b - a, c - b, d - c, e - d];
 
-        for (seq, bananas) in seq_map.iter() {
+            if !seen.insert(seq) {
+                return;
+            }
+
             potential_earnings
-                .entry(*seq)
-                .and_modify(|b| *b += *bananas as u64)
-                .or_insert(*bananas as u64);
-        }
+                .entry(seq)
+                .and_modify(|b| *b += e as u64)
+                .or_insert(e as u64);
+        });
     }
 
     potential_earnings.values().max().unwrap().to_string()
 }
 
-// Calculates the potential earnings for each sequence of 4 price differences in
-// the data.
-pub fn sequences_and_earnings(differences: &[i8], prices: &[u8]) -> HashMap<[i8; 4], u8> {
-    differences
-        .windows(4)
-        // We need to skip the first window, because there is no difference before the first price
-        .skip(1)
-        .enumerate()
-        .fold(HashMap::new(), |mut map, (i, w)| {
-            let mut seq = [0; 4];
-            seq.copy_from_slice(w);
-
-            if !map.contains_key(&seq) {
-                // Since windows() goes forward in time instead of backward, we
-                // need to add 4 to the index to get the correct price
-                map.insert(seq, prices[i + 4]);
-            }
-
-            map
-        })
-}
-
 // Calculates the prices for a given seed.
-pub fn prices(seed: u64) -> Vec<u8> {
+pub fn prices(seed: u64) -> impl Iterator<Item = i8> {
     std::iter::successors(Some(seed), |&seed| Some(hash(seed)))
         .take(2000)
-        .map(|x| (x % 10) as u8)
-        .collect()
-}
-
-// Calculates the price differences for a given sequence of prices. Note that
-// the difference sequence must start with an arbitrary padding value to line up
-// the indices with the prices.
-pub fn price_differences(prices: &[u8]) -> Vec<i8> {
-    std::iter::once(0)
-        .chain(prices.windows(2).map(|w| w[1] as i8 - w[0] as i8))
-        .collect()
+        .map(|x| (x % 10) as i8)
 }
 
 #[cfg(test)]
