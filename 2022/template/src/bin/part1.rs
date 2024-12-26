77abba710291d9660646edc720fd5983f94cb138
Johannes Holzfuß <johannes@holzfuss.name>
2024-12-26 12:17:30 +0100

    2022: Update template

---
+++ 2022/template/src/bin/part1.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let puzzle_input = {{crate_name}}::parser::part1(include_str!("../../input.txt"));
+    println!("Part 1: {}", {{crate_name}}::p1::part1(&puzzle_input));
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_part1() {
+        let puzzle_input = {{crate_name}}::parser::part1(include_str!("../../input.txt"));
+        assert_eq!({{crate_name}}::p1::part1(&puzzle_input), "TODO");
+    }
+}
--- 2022/template/src/parser.rs
+++ 2022/template/src/parser.rs
@@ -1,27 +1,21 @@
 use utility_belt::prelude::*;
 
-use crate::structs::*;
-
-fn nom_parser(input: &str) -> IResult<&str, PuzzleInput> {
-    todo!();
-    let (input, _) = eof(input)?;
-
-    Ok((input, PuzzleInput {}))
+#[derive(Clone, Debug)]
+pub struct PuzzleInput {
+    // Remember to make the fields pub
 }
 
-pub fn parse(input: &str) -> PuzzleInput {
-    nom_parser(input).unwrap().1
+pub fn part1(input: &'static str) -> PuzzleInput {
+    // Checklist:
+    //
+    // 1. Can this be parsed using parse_ints(input)
+    // 2. Can this be parsed using a regular expression
+    // 3. Winnow parser?
+    // 4. ???
+
+    PuzzleInput{}
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use utility_belt::prelude::*;
-
-    const TEST_INPUT: &str = include_str!("../test.txt");
-
-    #[test]
-    fn test_parse() {
-        assert!(nom_parser(TEST_INPUT).is_ok());
-    }
+pub fn part2(input: &'static str) -> PuzzleInput {
+    part1(input)
 }
