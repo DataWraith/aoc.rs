5d1194135a0bdd4bbb99f93dc77a596ab6b0eeae
Johannes Holzfuß <johannes@holzfuss.name>
2024-12-27 11:48:20 +0100

    Day 17: Comment things

--- 2022/day-17/src/p2.rs
+++ 2022/day-17/src/p2.rs
@@ -3,9 +3,11 @@ use crate::{p1::Well, parser::*};
 pub fn part2(input: &PuzzleInput) -> String {
     let well = Well::new(input.rocks.clone(), input.jets.clone());
 
+    // Find the cycle start, cycle length, and the well state at the start of the first cycle
     let (cycle_length, mut well, start) =
         pathfinding::directed::cycle_detection::brent(well, |well| well.drop_rock());
 
+    // Then, compute the height difference between the start and end of the cycle
     let start_height = well.max_height as usize;
 
     for _ in 0..cycle_length {
@@ -15,21 +17,36 @@ pub fn part2(input: &PuzzleInput) -> String {
     let end_height = well.max_height as usize;
     let difference = end_height - start_height;
 
-    let cycles_to_run = (1000000000000 - start) / cycle_length;
+    // Compute the number of cycles to run.
+    //
+    // We subtract `start` and the cycle length because we already ran `start`
+    // steps in order to find the cycle, and then `cycle_length` steps in order
+    // to find the height-difference of a cycle.
+    let cycles_to_run = (1000000000000 - start - cycle_length) / cycle_length;
+
+    // This gives us the additional height from the cycles
     let cycle_height = cycles_to_run * difference;
 
-    let preliminary = cycle_height + start_height - difference;
+    // And of course, we need to add the start height back in
+    let mut total_height = start_height + cycle_height;
 
+    // Now we need to compute the remaining cycles to reach the 1000000000000-th
+    // step, again subtracting the steps we already ran.
     let remaining = (1000000000000 - start - cycle_length) % cycle_length;
-    let w = well.max_height as usize;
+
+    // And actually run the remaining cycles and get the height difference
+    let h_before = well.max_height as usize;
 
     for _ in 0..remaining {
         well = well.drop_rock();
     }
 
-    let w2 = well.max_height as usize;
+    let h_after = well.max_height as usize;
 
-    (preliminary + w2 - w).to_string()
+    // Add the height difference from the remaining steps to get our final answer
+    total_height += h_after - h_before;
+
+    total_height.to_string()
 }
 
 #[cfg(test)]
