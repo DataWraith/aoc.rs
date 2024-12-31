13d30e4dafedb6d639e694d687501730ff263654
Johannes Holzfuß <johannes@holzfuss.name>
2024-12-31 16:43:27 +0100

    Day 04: Part 1

---
+++ 2021/day-04/src/parser.rs
@@ -0,0 +1,34 @@
+use utility_belt::prelude::*;
+
+#[derive(Clone, Debug)]
+pub struct PuzzleInput {
+    // Remember to make the fields pub
+    pub numbers: Vec<i64>,
+    pub boards: Vec<Grid2D<i64>>,
+    pub marked: Vec<BoolGrid2D>,
+}
+
+pub fn part1(input: &'static str) -> PuzzleInput {
+    let (numbers, boards) = input.split_once("\n\n").unwrap();
+    let numbers = parse_ints(numbers);
+
+    let boards: Vec<Grid2D<i64>> = parse_ints(boards)
+        .chunks(25)
+        .map(|chunk| Grid2D::from_shape_vec(5, 5, chunk.to_vec()))
+        .collect();
+
+    let marked = boards
+        .iter()
+        .map(|grid| grid.map(|_| false).into())
+        .collect_vec();
+
+    PuzzleInput {
+        numbers,
+        boards,
+        marked,
+    }
+}
+
+pub fn part2(input: &'static str) -> PuzzleInput {
+    part1(input)
+}
