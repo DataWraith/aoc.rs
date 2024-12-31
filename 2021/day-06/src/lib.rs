14dd56fc570f4dcf0463f39789324a5734a29b41
Johannes Holzfuß <johannes@holzfuss.name>
2024-12-31 18:26:28 +0100

    Just for fun: Make lanternfish_sim a generator function

--- 2021/day-06/src/lib.rs
+++ 2021/day-06/src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(gen_blocks)]
+
 pub mod p1;
 pub mod p2;
 pub mod parser;
--- 2021/day-06/src/p1.rs
+++ 2021/day-06/src/p1.rs
@@ -10,18 +10,19 @@ pub fn lanternfish_after_days(fish: &[u8], days: usize) -> usize {
     let mut counter = Counter::from_iter(fish.iter().cloned());
 
     for _ in 0..days {
-        counter = state_iteration(&counter, |fish, _| lanternfish_sim(fish), ());
+        counter = state_iteration(&counter, |&fish, _| lanternfish_sim(fish), ());
     }
 
     counter.values().sum::<usize>()
 }
 
-pub fn lanternfish_sim(fish: &u8) -> Vec<u8> {
-    if *fish == 0 {
-        vec![6, 8]
+pub gen fn lanternfish_sim(fish: u8) -> u8 {
+    if fish == 0 {
+        yield 6;
+        yield 8;
     } else {
-        vec![fish - 1]
-    }
+        yield fish - 1;
+    };
 }
 
 #[cfg(test)]
