6c3085cee4a3368293550481a0b8b3875e4bfde2
Johannes Holzfuß <johannes@holzfuss.name>
2025-01-01 14:48:55 +0100

    Day 09: Part 2

--- 2021/day-09/src/bin/part2.rs
+++ 2021/day-09/src/bin/part2.rs
@@ -8,6 +8,6 @@ mod tests {
     #[test]
     fn test_part2() {
         let puzzle_input = day_09::parser::part2(include_str!("../../input.txt"));
-        assert_eq!(day_09::p2::part2(&puzzle_input), "TODO");
+        assert_eq!(day_09::p2::part2(&puzzle_input), "1047744");
     }
 }
--- 2021/day-09/src/p2.rs
+++ 2021/day-09/src/p2.rs
@@ -1,9 +1,56 @@
+use std::cmp::Reverse;
+
 use utility_belt::prelude::*;
 
 use crate::parser::*;
 
 pub fn part2(input: &PuzzleInput) -> String {
-    todo!("day_09::p2::part2");
+    let low_points = input
+        .map
+        .iter()
+        .filter_map(|(coord, height)| {
+            let is_low_point = coord
+                .neighbors()
+                .filter(|neighbor| input.map.contains(*neighbor))
+                .all(|neighbor| input.map[neighbor] > *height);
+
+            if is_low_point {
+                Some(coord)
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<_>>();
+
+    let mut basins = Vec::new();
+
+    for low_point in low_points {
+        let mut size = 0;
+
+        let mut q = VecDeque::new();
+        let mut visited = HashSet::new();
+        q.push_back(low_point);
+
+        while let Some(coord) = q.pop_front() {
+            if !visited.insert(coord) {
+                continue;
+            }
+
+            size += 1;
+
+            for neighbor in coord.neighbors() {
+                if input.map.contains(neighbor) && input.map[neighbor] < 9 {
+                    q.push_back(neighbor);
+                }
+            }
+        }
+
+        basins.push(size);
+    }
+
+    let (basins, _, _) = basins.select_nth_unstable_by_key(3, |&size| Reverse(size));
+
+    basins.iter().product::<usize>().to_string()
 }
 
 #[cfg(test)]
@@ -12,13 +59,17 @@ mod tests {
     use utility_belt::prelude::*;
 
     const TEST_INPUT: &str = indoc! {"
-        TODO
+        2199943210
+        3987894921
+        9856789892
+        8767896789
+        9899965678
     "};
 
     #[test]
     fn test_part2_example() {
         let input = crate::parser::part2(TEST_INPUT);
         assert_ne!(TEST_INPUT.trim(), "TODO");
-        assert_eq!(part2(&input), "TODO");
+        assert_eq!(part2(&input), "1134");
     }
 }
