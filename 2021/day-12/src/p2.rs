b354f1d1fa5de18944dd6f9be4c771a3d67b60b9
Johannes Holzfuß <johannes@holzfuss.name>
2025-01-04 08:53:48 +0100

    Day 12: Part 2

--- 2021/day-12/src/p2.rs
+++ 2021/day-12/src/p2.rs
@@ -3,7 +3,44 @@ use utility_belt::prelude::*;
 use crate::parser::*;
 
 pub fn part2(input: &PuzzleInput) -> String {
-    todo!("day_12::p2::part2");
+    let paths = cave_paths(input);
+    paths.len().to_string()
+}
+
+pub fn cave_paths(input: &PuzzleInput) -> Vec<Vec<&str>> {
+    let mut paths = Vec::new();
+    let mut q = VecDeque::new();
+
+    q.push_back((vec!["start"], false));
+
+    while let Some((current, small_cave_visited_twice)) = q.pop_front() {
+        let last = current.last().unwrap();
+
+        if *last == "end" {
+            paths.push(current);
+            continue;
+        }
+
+        for neighbor in input.cave.neighbors(last) {
+            if neighbor.chars().all(|c| c.is_ascii_lowercase()) && current.contains(&neighbor) {
+                if small_cave_visited_twice || neighbor == "start" {
+                    continue;
+                }
+
+                let mut new_path = current.clone();
+                new_path.push(neighbor);
+
+                q.push_back((new_path, true));
+            } else {
+                let mut new_path = current.clone();
+                new_path.push(neighbor);
+
+                q.push_back((new_path, small_cave_visited_twice));
+            }
+        }
+    }
+
+    paths
 }
 
 #[cfg(test)]
@@ -16,6 +53,6 @@ mod tests {
     fn test_part2_example() {
         let input = parser::part2(parser::TEST_INPUT);
         assert_ne!(parser::TEST_INPUT.trim(), "TODO");
-        assert_eq!(part2(&input), "TODO");
+        assert_eq!(part2(&input), "36");
     }
 }
